0b9b7a4ec9b2325bac57417fc2fc610ecb5bd1b7
sukritijain2430 <137172209+sukritijain2430@users.noreply.github.com>
2024-04-08 14:10:15 +0530

    sql_queries.sql

--- SQL script 3_Copy1.sql
+++ SQL script 3_Copy1.sql
@@ -9,10 +9,35 @@ where country in (select team_country from medals where rank between 1 and 5)
 group by Country,Discipline
 order by  Total_athletes desc;
 
+select Country,count(*) as Total_athletes from athletes
+where country in (select team_country from medals where rank between 1 and 5)
+group by Country
+order by  Total_athletes desc;
+
+select Country,count(*) as Total_athletes from athletes
+where country in (select team_country from medals where rank between 1 and 5)
+group by Country
+order by  Total_athletes desc;
+
 
 select country,count(*) no_of_coaches from coaches
 where country in (select team_country from medals where rank between 1 and 10)
 group by Country
 order by no_of_coaches desc;
+
+select country,discipline,count(*) no_of_coaches from coaches
+where country in (select team_country from medals where rank between 1 and 10)
+group by Discipline,country
+order by no_of_coaches desc;
+
+select Discipline,avg(male) as avg_male,
+avg(female) as avg_female from entriesgender
+group by discipline;
+
+select country,count(*) as total_team
+from teams
+group by country
+order by total_team desc;
+
 
 select team_country from medals where rank between 1 and 5
